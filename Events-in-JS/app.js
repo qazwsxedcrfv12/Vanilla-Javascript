78bf78472ec9572bdb4a87a53e82ddc3f97e9d95
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-05-21 21:31:35 +0530

    Add files via upload

---
+++ Events-in-JS/app.js
@@ -0,0 +1,72 @@
+
+
+document.querySelector('#btn').addEventListener('click',getData)
+document.querySelector("#btn").addEventListener('mouseenter',function(e){
+    const a=e.target
+    a.style.color="white"
+    a.style.background="black"
+   
+    const b=null
+    setTimeout(function(){
+        a.style.color=b;
+        a.style.background=b;
+    },800)
+    // console.log(a)
+})
+
+function clearFields(){
+    document.getElementById('name').value='';
+    document.getElementById('email').value='';
+    document.getElementById('phone').value='';
+    document.getElementById('address').value='';
+}
+
+function showAlert(className,message)
+{
+     const div=document.createElement('div')
+     div.className=`alert ${className}`
+     div.appendChild(document.createTextNode(message));
+     // Get parent
+     const container = document.querySelector('.container');
+     // Get form
+     const form = document.querySelector('table');
+     // Insert alert
+     container.insertBefore(div, form);
+   
+     // Timeout after 1 sec
+     setTimeout(function(){
+       document.querySelector('.alert').remove();
+     }, 2000);
+}
+
+function getData(e){
+
+    
+    const name=document.getElementById('name').value;
+    const email=document.getElementById('email').value;
+    const phone=document.getElementById('phone').value;
+    const address=document.getElementById('address').value;
+    
+
+    if(name === '' || email === '' || phone === '' || address === ''){
+        showAlert('failure','Please fill in all details')
+    }
+    else{
+        const data=document.querySelector('.data');
+    
+        const row=document.createElement('tr')
+        
+        row.innerHTML=`
+        <td>${name}</td>
+        <td>${email}</td>
+        <td>${phone}</td>
+        <td>${address}</td>
+        <td><a href="#">X</a></td>
+        `;
+        
+        data.appendChild(row);
+        clearFields();
+        showAlert('success','Person Details Added');
+    }
+     e.preventDefault();
+}
