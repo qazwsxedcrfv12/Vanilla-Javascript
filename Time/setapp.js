5939a7344b84731a1309dc3ece6ffd7855801dd9
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-04 00:04:47 +0530

    Update setapp.js

--- Time/setapp.js
+++ Time/setapp.js
@@ -2,11 +2,21 @@
 
 document.getElementById('btn').addEventListener('click',getTime)
 
+document.getElementById('hide').addEventListener('click',hideData)
+
+//-----------------------------------------------------------------------------
+
+var timeValue=null;
 
 function getTime(){
-    setInterval(function(){
+     timeValue = setInterval(function(){
         const time=new Date();
         document.getElementById('time').innerHTML=time;
-    },1000)
+    },1000);
     
+}
+
+function hideData(){
+    document.getElementById('time').innerHTML=" "
+    clearInterval(timeValue)
 }
