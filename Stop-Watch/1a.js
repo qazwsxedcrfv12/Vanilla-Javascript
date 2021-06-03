41eaf16e672ad38e9b72d04e3348e6e6e27cb598
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-03 12:17:39 +0530

    Update 1a.js

--- Stop-Watch/1a.js
+++ Stop-Watch/1a.js
@@ -9,50 +9,62 @@ document.getElementById('reset').addEventListener('click',resetData)
 
 var timeValue = null;
 
+function setData(t,h,i,j){
+
+  let div=document.querySelector('.item');
+ 
+  timeValue = setInterval(() => {
+    
+    j++;
+
+    if(j === 100)
+    { i++; j=0; }
+    if(i === 60)
+    { h++; i=0;}
+    
+    if(h<10 && i<10 && j<10)
+      div.innerHTML=`0${h}:0${i}:0${j}`;
+    else if(i<10 && h<10)
+      div.innerHTML=`0${h}:0${i}:${j}`;
+    else if(h<10 && j<10)
+      div.innerHTML=`0${h}:${i}:0${j}`;
+    else if(j<10 && i<10)
+      div.innerHTML=`${h}:0${i}:0${j}`;
+    else if(j<10)
+      div.innerHTML=`${h}:${i}:0${j}`;
+    else if(i<10)
+      div.innerHTML=`${h}:0${i}:${j}`;
+    else if(h<10)
+      div.innerHTML=`0${h}:${i}:${j}`;
+    else
+      div.innerHTML=`${h}:${i}:${j}`;
+
+    div.style.width="100px";
+    div.style.padding="13px"
+    div.style.border="1px double black";
+    div.style.borderRadius="23px"
+    div.style.background="#cfcbcb"
+
+},t);
+
+}
 
 function getData(){                           //It will start the stopwatch
-
-    let i=0,h=0,j=0;
-   
-    let div=document.querySelector('.item');
-   
-    timeValue = setInterval(() => {
-      
-      j++;
-  
-      if(j === 100)
-      { i++; j=0; }
-      if(i === 60)
-      { h++; i=0;}
-      
-      if(h<10 && i<10)
-        div.innerHTML=`0${h}:0${i}:${j}`;
-      else if(i<10)
-        div.innerHTML=`${h}:0${i}:${j}`;
-      else if(h<10)
-        div.innerHTML=`0${h}:${i}:${j}`;
-      else if(j<10)
-        div.innerHTML=`${h}:${i}:0${j}`;
-      else
-        div.innerHTML=`${h}:${i}:${j}`;
-  
-      div.style.width="100px";
-      div.style.padding="13px"
-      div.style.border="1px double black";
-      div.style.borderRadius="23px"
-      div.style.background="#cfcbcb"
-  
-  },10);
+    
+   setData(10,0,0,0);
 }
 //---------------------------------------------------------------------------------------------
 
 function stopData(){                     //It will pause the stopwatch
-    clearInterval(timeValue);
+    
+   clearInterval(timeValue);
 }
 
 
 //---------------------------------------------------------------------------------------------
 function resetData(){                   //It will reset the value of stopwatch
+   
    clearInterval(timeValue);
+   
    document.querySelector('.item').innerHTML="00:00:00";
 }
